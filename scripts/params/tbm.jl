bfe7f3b1f0b35713fd6d33f42f676daa04ef4ee8
Davis Thuillier <dsthuillier@gmail.com>
2023-07-10 14:35:17 -0700

    Made calls to hamiltonian explicit

--- scripts/params/tbm.jl
+++ scripts/params/tbm.jl
@@ -1,6 +1,7 @@
 ### Example Band ###
 
-const mu::Float64 = -1.0
+const mu::Float64 = 0.2
 const tx::Float64 = 1.0
 const ty::Float64 = 1.0
-const tp::Float64 = 0
+
+hamiltonian(k::SVector{2, Float64}) = - 2.0 * tx * cos(k[1]*pi) - 2.0 * ty * cos(k[2]*pi) - mu
